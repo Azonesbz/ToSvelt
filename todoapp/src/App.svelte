cf46bc36ab4d8c3cf3c64244f08284b17d317ac0
Azones <59911700+Azonesbz@users.noreply.github.com>
2023-09-28 18:54:48 +0200

    first commit

---
+++ todoapp/src/App.svelte
@@ -0,0 +1,16 @@
+<script>
+  import Taskcontainer from "./lib/Taskcontainer.svelte";
+</script>
+
+<head>
+  <link rel="stylesheet" href="/app.css">
+</head>
+
+<main class="min-h-screen h-screen flex items-center justify-center">
+  <nav class="fixed min-w-screen w-full h-[75px] flex items-center justify-center inset-0">
+    <h1>todoapp</h1>
+  </nav>
+  <section class="h-[80vh] w-full flex items-center justify-center px-[200px]">
+    <Taskcontainer/>
+  </section>
+</main>
---
+++ todoapp/tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  purge: ['./src/**/*.svelte', './index.html'],
+  content: [],
+  theme: {
+    extend: {
+      fontFamily: {
+        Signika: ['Signika Negative']
+      }
+    },
+  },
+  plugins: [],
+}
+
