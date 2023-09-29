560a579a5c24e474d0267a4b690e9680767023b1
Azones <59911700+Azonesbz@users.noreply.github.com>
2023-09-29 12:04:37 +0200

    created button component

---
+++ todoapp/src/lib/Button.svelte
@@ -0,0 +1,19 @@
+<script>
+    export let onClick;
+    export let disable
+    export let label;
+    export let side = "middle" || "left" || "right"
+</script>
+  
+<button 
+    class="px-4 py-2 text-white font-semibold rounded w-full"
+    class:bg-green-800={label === "Valider"} 
+    class:bg-red-800={label === "Supprimer"} 
+    class:bg-yellow-800={label === "Modifier"} 
+    class:rounded-l-3xl={side === "left"} 
+    class:rounded-r-3xl={side === "right"} 
+    on:click={onClick}
+    disabled={disable}
+>
+    {label}
+</button>
